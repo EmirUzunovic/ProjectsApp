e285eca003ed01789743254cec44d9d35b945f56
Emir Uzunovic <emir.uzunovic@gmail.com>
2019-03-13 13:37:22 +0100

    adding cloud functions for notification, listeners are projects/{projectID} created and users/{usersId} created

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,6 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin')
+admin.initializeApp(functions.config().firebase)
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
@@ -7,8 +9,35 @@ const functions = require('firebase-functions');
 //  response.send("Hello from Firebase!");
 // });
 
-exports.testFunction = functions.https.onRequest((request, response) => {
-    response.send("Hello from Firebase!")
-});
+const createNotification = (notification => {
+    return admin.firestore().collection('notifications')
+                            .add(notification)
+                            .then( doc => console.log('notification added', doc))
+})
 
+exports.projectCreated = functions.firestore
+       .document('projects/{projectId}')
+       .onCreate(doc => {
+                            const project = doc.data();
+                            const notification = {
+                                content: 'Added a new project', 
+                                user : `${project.authorFirstName} ${project.authorLastName}`,
+                                time: admin.firestore.FieldValue.serverTimestamp()
+                                    }
+                                return createNotification(notification)  
+                        }
+                    );
+
+exports.NewUserJoined = functions.firestore
+                        .document('users/{userId}')
+                        .onCreate(doc => {
+                                                const newUser = doc.data();
+                                                const notification = {
+                                                    content: 'New user joined', 
+                                                    user : `${newUser.firstName} ${newUser.lastName}`,
+                                                    time: admin.firestore.FieldValue.serverTimestamp()
+                                                        }
+                                                    return createNotification(notification)  
+                                            }
+                                        );
 
