2c84d2bbcb93bd130df343fc6e0ca4df1c5ae0af
Emir Uzunovic <emir.uzunovic@gmail.com>
2019-03-19 13:00:35 +0100

    build

---
+++ dist/precache-manifest.d8ab4d25b6b88aaf238625cc0ff97735.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "502bd35f84fbaf478b7e",
+    "url": "/static/css/main.b0340373.chunk.css"
+  },
+  {
+    "revision": "502bd35f84fbaf478b7e",
+    "url": "/static/js/main.502bd35f.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "0431d91c8aa6d6eed06e",
+    "url": "/static/css/2.51d11bec.chunk.css"
+  },
+  {
+    "revision": "0431d91c8aa6d6eed06e",
+    "url": "/static/js/2.0431d91c.chunk.js"
+  },
+  {
+    "revision": "6561cab35d787df0435b74a5c3c9d21d",
+    "url": "/index.html"
+  }
+];
